b720bb9b9d4acc9a6c6568e8fbcc1b07a052567f
Radionov_Vitaly <radionovv89@gmail.com>
2023-01-01 23:39:16 +0300

    removeFalsyValues should return the specified array without falsy values

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -100,8 +100,8 @@ function getArrayOfStrings(arr) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((element) => element !== [false, null, 0, '', undefined].find((item) => item === element) && !Number.isNaN(element));
 }
 
 /**
