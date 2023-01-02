1b0d11903f46fe41bbf92eb70ddbc624a32db681
Radionov_Vitaly <radionovv89@gmail.com>
2023-01-02 23:29:43 +0300

    swapHeadAndTail should swap the head and tail of the array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -576,8 +576,10 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const head = arr.splice(0, Math.floor(arr.length / 2));
+  const tail = head.length < arr.length ? arr.splice(1, arr.length) : arr.splice(0, arr.length);
+  return [].concat(tail, arr, head);
 }
 
 
