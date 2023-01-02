e2554b568dffec351a2c3ef2c60ecf28994457e0
Radionov_Vitaly <radionovv89@gmail.com>
2023-01-03 01:28:50 +0300

    timeSpanToString should return the string represation of time span between two dates

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -73,8 +73,12 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const ms = endDate - startDate;
+  const s = ((ms / 1000) % 60).toFixed(3) || 0;
+  const m = Math.floor((ms / 1000) / 60) % 60 || 0;
+  const h = Math.floor((ms / 1000) / (60 * 60)) % 24 || 0;
+  return `${h > 9 ? h : `0${h}`}:${m > 9 ? m : `0${m}`}:${s > 9 ? s : `0${s}`}`;
 }
 
 
