e7213541efce8608afd5d67dc4bb99ec704aa438
Sam Gold <Radionovv89@gmail.com>
2023-05-08 20:24:21 +0300

    feat: add solution for doRectanglesOverlap

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -141,8 +141,14 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const top1 = rect1.top + rect1.height;
+  const left1 = rect1.left + rect1.width;
+
+  if ((rect2.top > top1) || (rect2.left > left1)) {
+    return false;
+  }
+  return true;
 }
 
 
