494e9911272f015459b87f914eaf37b42fac98c7
Sam Gold <Radionovv89@gmail.com>
2023-05-08 20:10:28 +0300

    feat: add solution for isTriangle

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -100,8 +100,12 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  const max = Math.max(a, b, c);
+  if (max < ((a + b + c) - max)) {
+    return true;
+  }
+  return false;
 }
 
 
