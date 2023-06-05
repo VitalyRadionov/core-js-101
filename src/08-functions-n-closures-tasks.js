3b0642284c2ad60f2a69e7716c98f31b0001cb4b
Sam Gold <Radionovv89@gmail.com>
2023-06-05 12:31:52 +0300

    feat: add solution for retry

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -111,8 +111,17 @@ function memoize(fn) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        func();
+      } catch (err) {
+        Error();
+      }
+    }
+    return func();
+  };
 }
 
 
